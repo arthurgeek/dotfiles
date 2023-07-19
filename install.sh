571d27fb25e452c5cc08ee9a2279181cfeb8c1f3
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2023-07-19 23:14:24 +0200

    feat: adds install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eufo pipefail
+
+# Install Homebrew
+command -v brew >/dev/null 2>&1 || \
+  (echo "🍺 Installing Homebrew" && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+# Install chezmoi
+command -v chezmoi >/dev/null 2>&1 || \
+  (echo "🏠 Installing chezmoi" && brew install chezmoi)
+
+# Initialize chezmoi
+echo "🚀 Initializing chezmoi" && chezmoi init --branch chezmoi https://github.com/arthurgeek/dotfiles
+
+# Initialize chezmoi
+echo "🪄 Applying dotfiles" && chezmoi apply
+
+echo ""
+echo "🏁 Done."
