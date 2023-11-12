e9df2db94d633a15c3f6c3008c836dafdc17c44f
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2023-11-12 21:26:12 +0100

    feat: wezterm config

---
+++ dot_wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Dracula (Official)"
+config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Bold' })
+config.font_size = 16.0
+
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+
+config.window_padding = {
+  top = '1.5cell',
+}
+
+local act = wezterm.action
+
+config.keys = {
+  {
+    key = 'k',
+    mods = 'CMD',
+    action = act.Multiple {
+      act.ClearScrollback 'ScrollbackAndViewport',
+    },
+  },
+}
+
+return config
