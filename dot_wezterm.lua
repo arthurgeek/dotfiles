418ed410dedefe1bf815cc3de732dc87392ea7bf
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2023-11-13 16:31:20 +0100

    chore: small changes to wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,9 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Dracula (Official)"
-config.font = wezterm.font('FiraCode Nerd Font', { weight = 'Bold' })
+config.color_scheme = 'Dracula (Official)'
+config.font = wezterm.font('FiraCode Nerd Font Mono')
 config.font_size = 16.0
+config.freetype_load_target = 'HorizontalLcd'
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 
@@ -13,7 +14,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
 config.window_padding = {
+  left = '0.5cell',
+  right = '0.5cell',
   top = '1.5cell',
+  bottom = '0'
 }
 
 local act = wezterm.action
