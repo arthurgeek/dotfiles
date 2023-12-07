94f2ee59fb852ce12867a673a866bee8bcaf6ea5
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2023-12-07 16:58:41 +0100

    feat(fish): adds golang config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,8 +1,13 @@
 set -gx fish_greeting
 set -gx ANSIBLE_FORCE_COLOR true
 
+# GOLANG configurations
+set -gx GOPATH $HOME/Development/go
+set -gx GOROOT /opt/homebrew/opt/go/libexec
+set -gx PATH $GOPATH/bin $GOROOT/bin $PATH
+
 if status is-interactive
-    eval (/opt/homebrew/bin/brew shellenv)
+  eval (/opt/homebrew/bin/brew shellenv)
 end
 
 direnv hook fish | source
