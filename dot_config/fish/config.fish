e83eb2fdbbc500c34812947418b2e4786f6b4329
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2023-12-09 20:41:47 +0100

    feat: adds catpuccing mocha theme
    for wezterm, fish and bat

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -11,11 +11,13 @@ if status is-interactive
   eval (/opt/homebrew/bin/brew shellenv)
 end
 
+fish_config theme choose "Catppuccin Mocha"
+
 direnv hook fish | source
 starship init fish | source
 zoxide init fish | source
 thefuck --alias | source
-abbr --global cat "bat --theme Dracula"
+abbr --global cat "bat --theme Catppuccin-mocha"
 abbr --global f fuck
 abbr --global k kubectl
 abbr --global vim nvim
--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Dracula (Official)'
+config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font('FiraCode Nerd Font')
 config.font_size = 18.0
 config.freetype_load_target = 'HorizontalLcd'
