2cb42142610369bea89e19a7461876c87498d1b3
Arthur Zapparoli <2992+arthurgeek@users.noreply.github.com>
2023-12-11 19:59:23 +0100

    feat(fish): exa config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,6 +7,11 @@ set -gx GOPATH $HOME/Development/go
 set -gx GOROOT /opt/homebrew/opt/go/libexec
 set -gx PATH $GOPATH/bin $GOROOT/bin $PATH
 
+# EXA plugin config
+set EXA_STANDARD_OPTIONS --group --header --group-directories-first --icons
+set EXA_L_OPTIONS $EXA_STANDARD_OPTIONS
+set EXA_LT_OPTIONS --long --tree --level
+
 if status is-interactive
     eval (/opt/homebrew/bin/brew shellenv)
 end
@@ -17,8 +22,10 @@ direnv hook fish | source
 starship init fish | source
 zoxide init fish | source
 thefuck --alias | source
+
 abbr --global cat "bat --theme Catppuccin-mocha"
 abbr --global f fuck
 abbr --global k kubectl
 abbr --global vim nvim
 abbr --global vi nvim
+abbr --global ls l
