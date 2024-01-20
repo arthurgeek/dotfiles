9a1285495806a50a9eebaef657e7226198ca343d
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2024-01-20 17:30:18 +0100

    feat(nvim): adds project.nvim
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -31,6 +31,7 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.lsp.none-ls' },
     { import = 'lazyvim.plugins.extras.test.core' },
     { import = 'lazyvim.plugins.extras.ui.mini-animate' },
+    { import = 'lazyvim.plugins.extras.util.project' },
     -- import/override with your plugins
     { import = 'plugins' },
   },
---
+++ dot_config/nvim/lua/plugins/project.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'ahmedkhalf/project.nvim',
+    opts = {
+      manual_mode = false,
+      silent_chdir = false,
+    },
+  },
+}
