7303877acad857dad402affc1130797bcbb0c8cd
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2024-01-20 17:05:51 +0100

    feat(fish/fzf): configure fzf fish plugin
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -16,6 +16,10 @@ set EXA_STANDARD_OPTIONS --group --git --header --group-directories-first --icon
 set EXA_L_OPTIONS $EXA_STANDARD_OPTIONS
 set EXA_LT_OPTIONS --long --tree --level
 
+# FZF plugin config
+set fzf_preview_dir_cmd eza $EXA_STANDARD_OPTIONS --all --oneline --color=always
+set fzf_diff_highlighter delta --features="default decorations" --paging=never
+set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
 
 fish_config theme choose "Catppuccin Mocha"
 
