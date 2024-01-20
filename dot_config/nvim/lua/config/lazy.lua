27482c7bcd920b73566a50e7f7eaed86ffa8de52
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2024-01-20 17:32:34 +0100

    feat(nvim): adds copilot
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.yaml' },
     { import = 'lazyvim.plugins.extras.coding.yanky' },
+    { import = 'lazyvim.plugins.extras.coding.copilot' },
     { import = 'lazyvim.plugins.extras.dap.core' },
     { import = 'lazyvim.plugins.extras.ui.edgy' },
     { import = 'lazyvim.plugins.extras.editor.symbols-outline' },
