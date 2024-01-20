fbc7c2c1181eec2b5528754307b812ea753233f7
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2024-01-20 17:32:24 +0100

    feat(nvim): adds terraform
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.lang.json' },
     { import = 'lazyvim.plugins.extras.lang.markdown' },
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
+    { import = 'lazyvim.plugins.extras.lang.terraform' },
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.yaml' },
     { import = 'lazyvim.plugins.extras.coding.yanky' },
