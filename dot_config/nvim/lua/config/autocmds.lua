a4d106e2c7247e2ff2cc797289bbe4f7734a90bb
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2024-01-20 17:29:01 +0100

    feat(nvim): auto apply chezmoi when
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Auto apply chezmoi when saving
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  group = vim.api.nvim_create_augroup('auto_apply_chezmoi', { clear = true }),
+  pattern = '~/.local/share/chezmoi/*',
+  command = '!chezmoi apply --source-path %',
+})
