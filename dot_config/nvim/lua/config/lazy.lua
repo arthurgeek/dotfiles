88f75c08997f4d43e352d8351149c6c915734e87
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2024-03-07 19:59:46 +0100

    updates to lazyvim
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -28,7 +28,7 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.coding.copilot' },
     { import = 'lazyvim.plugins.extras.dap.core' },
     { import = 'lazyvim.plugins.extras.ui.edgy' },
-    { import = 'lazyvim.plugins.extras.editor.symbols-outline' },
+    { import = 'lazyvim.plugins.extras.editor.outline' },
     { import = 'lazyvim.plugins.extras.formatting.prettier' },
     { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.test.core' },
